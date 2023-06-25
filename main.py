b27da227034858433f23b1eebfc87b0f68f77175
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-25 13:21:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,57 +1,81 @@
 import streamlit as st
 import pandas as pd
-from openai import OpenAI
+import openai
 
-# Set up your GPT-3 API key
-openai = OpenAI("your_api_key_here")
+def classify_page_type(url: str, title: str, meta_description: str) -> str:
+    # Set your OpenAI API key
+    openai.api_key = 'your-openai-api-key'
 
-# Define your prompts
-prompts = {
-    'PDP': 'Product description for {}',
-    'Category Page': 'Description for category page {}',
-    'Blog Article': 'Summary for blog article {}',
-    'About Page': 'About us content for {}',
-    # Add more page types and prompts as needed
-}
+    # List of possible page types
+    page_types = ['Home Page', 'Product Detail Page', 'Category Page', 
+                  'About Us Page', 'Contact Us Page', 'Blog Article Page',
+                  'Services Page', 'Landing Page', 'Privacy Policy Page', 
+                  'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
+                  'Portfolio Page', 'Case Study Page', 'Press Release Page', 
+                  'Events Page', 'Resources/Downloads Page', 'Team Members Page',
+                  'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
+                  'Forum/community page', 'News Page']
 
-def classify_page_type(url, title, meta_description):
-    # Implement your page type classification logic here
-    # For the purpose of this example, this function just returns 'PDP'
-    return 'PDP'
+    # Generate the prompt string
+    page_types_prompt = ', '.join(page_types)
 
-def generate_description(prompt):
-    # Use the OpenAI API to generate a description based on the prompt
-    response = openai.complete(prompt=prompt)
+    # Generate the text completion
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f"The URL is: {url}\nThe title is: {title}\nThe meta description is: {meta_description}\nGiven the following page types: {page_types_prompt}, the page type is",
+        temperature=0.3,
+        max_tokens=60
+    )
+
+    # Return the generated text
     return response.choices[0].text.strip()
 
-def main():
-    st.title('SEO Content Generator')
-    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
-    if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file)
-        data['Generated Description'] = ''
-        st.write(data)
-        fill_empty_meta = st.checkbox('Only generate descriptions for rows with empty meta descriptions')
-        if st.button('Generate Content'):
-            # Iterate over each row in the data
-            for idx, row in data.iterrows():
-                # Only consider URLs with status code 200 and that are "indexable"
-                if row['Status Code'] != 200 or row['Indexability'] != 'indexable':
-                    continue
-                # If user selected to fill empty meta descriptions, skip rows that already have a meta description
-                if fill_empty_meta and pd.notnull(row['Meta Description']):
-                    continue
-                # Use GPT-3 to classify page type
-                page_type = classify_page_type(row['URL'], row['Title'], row['Meta Description'])
-                # Select the appropriate prompt for the page type
-                if page_type in prompts:
-                    prompt = prompts[page_type].format(row['URL'])
-                    # Generate the description
-                    description = generate_description(prompt)
-                    # Store the generated description in the dataframe
-                    data.at[idx, 'Generated Description'] = description
-            st.write(data)
+def generate_description(prompt: str) -> str:
+    # Set your OpenAI API key
+    openai.api_key = 'your-openai-api-key'
+    
+    # Generate the text completion
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        temperature=0.3,
+        max_tokens=60
+    )
+    
+    # Return the generated text
+    return response.choices[0].text.strip()
 
-if __name__ == "__main__":
-    main()
+def process_data(df: pd.DataFrame) -> pd.DataFrame:
+    # Filter based on 'Status Code' and 'Indexability'
+    df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'Indexable')]
+
+    # Classify page types
+    df['PageType'] = df.apply(lambda row: classify_page_type(row['Address'], row['Title 1'], row['Meta Description 1']), axis=1)
+    
+    # Define prompts for each page type.
+    prompts = {
+        "Home Page": "Write a compelling meta description for a Home Page with title '{title}' and url '{url}'",
+        "Product Detail Page": "Write a compelling meta description for a Product Detail Page with title '{title}' and url '{url}'",
+        # Add other page types here
+    }
+    
+    # Generate descriptions for each page type
+    for page_type, prompt_template in prompts.items():
+        df.loc[df['PageType'] == page_type, 'Generated Meta Description'] = df[df['PageType'] == page_type].apply(
+            lambda row: generate_description(prompt_template.format(title=row['Title 1'], url=row['Address'])), 
+            axis=1
+        )
+    
+    return df
+
+def download_link(object_to_download, download_filename, download_link_text):
+    # Generate a download link for the processed dataframe
+    if isinstance(objectSure, here is how you can continue the code:
+
+```python
+        if st.button('Download Data'):
+            csv = data.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+            href = f'<a href="data:file/csv;base64,{b64}" download="processed_data.csv">Download Processed CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
     
