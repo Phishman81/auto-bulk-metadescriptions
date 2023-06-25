bd22531259fa1e2dafa6aa76eff666ebf1a812f7
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-25 23:55:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import openai
+import base64
 
 # Get the password and api key from the secrets
 correct_password = st.secrets['password']
@@ -14,91 +15,94 @@ if password == correct_password:
     # If the password is correct, then the user can use the app
     st.title('Your Streamlit App')
     
-    # Your code here
+    # Function to classify page type
+    def classify_page_type(url: str, title: str, meta_description: str) -> str:
+        # Set your OpenAI API key
+        openai.api_key = api_key
 
+        # List of possible page types
+        page_types = ['Home Page', 'Product Detail Page', 'Category Page', 
+                      'About Us Page', 'Contact Us Page', 'Blog Article Page',
+                      'Services Page', 'Landing Page', 'Privacy Policy Page', 
+                      'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
+                      'Portfolio Page', 'Case Study Page', 'Press Release Page', 
+                      'Events Page', 'Resources/Downloads Page', 'Team Members Page',
+                      'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
+                      'Forum/community page', 'News Page']
 
+        # Generate the prompt string
+        page_types_prompt = ', '.join(page_types)
 
-----
-import streamlit as st
-import pandas as pd
-import openai
+        # Generate the text completion
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"The URL is: {url}\nThe title is: {title}\nThe meta description is: {meta_description}\nGiven the following page types: {page_types_prompt}, the page type is",
+            temperature=0.3,
+            max_tokens=60
+        )
 
-def classify_page_type(url: str, title: str, meta_description: str) -> str:
-    # Set your OpenAI API key
-    openai.api_key = 'your-openai-api-key'
+        # Return the generated text
+        return response.choices[0].text.strip()
 
-    # List of possible page types
-    page_types = ['Home Page', 'Product Detail Page', 'Category Page', 
-                  'About Us Page', 'Contact Us Page', 'Blog Article Page',
-                  'Services Page', 'Landing Page', 'Privacy Policy Page', 
-                  'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
-                  'Portfolio Page', 'Case Study Page', 'Press Release Page', 
-                  'Events Page', 'Resources/Downloads Page', 'Team Members Page',
-                  'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
-                  'Forum/community page', 'News Page']
+    # Function to generate description
+    def generate_description(prompt: str) -> str:
+        # Set your OpenAI API key
+        openai.api_key = api_key
+        
+        # Generate the text completion
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.3,
+            max_tokens=60
+        )
+        
+        # Return the generated text
+        return response.choices[0].text.strip()
 
-    # Generate the prompt string
-    page_types_prompt = ', '.join(page_types)
+    # Function to process data
+    def process_data(df: pd.DataFrame) -> pd.DataFrame:
+        # Filter based on 'Status Code' and 'Indexability'
+        df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'Indexable')]
 
-    # Generate the text completion
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=f"The URL is: {url}\nThe title is: {title}\nThe meta description is: {meta_description}\nGiven the following page types: {page_types_prompt}, the page type is",
-        temperature=0.3,
-        max_tokens=60
-    )
-
-    # Return the generated text
-    return response.choices[0].text.strip()
-
-def generate_description(prompt: str) -> str:
-    # Set your OpenAI API key
-    openai.api_key = 'your-openai-api-key'
-    
-    # Generate the text completion
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        temperature=0.3,
-        max_tokens=60
-    )
-    
-    # Return the generated text
-    return response.choices[0].text.strip()
-
-def process_data(df: pd.DataFrame) -> pd.DataFrame:
-    # Filter based on 'Status Code' and 'Indexability'
-    df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'Indexable')]
-
-    # Classify page types
-    df['PageType'] = df.apply(lambda row: classify_page_type(row['Address'], row['Title 1'], row['Meta Description 1']), axis=1)
-    
-    # Define prompts for each page type.
-    prompts = {
-        "Home Page": "Write a compelling meta description for a Home Page with title '{title}' and url '{url}'",
-        "Product Detail Page": "Write a compelling meta description for a Product Detail Page with title '{title}' and url '{url}'",
-        # Add other page types here
-    }
+        # Classify page types
+        df['PageType'] = df.apply(lambda row: classify_page_type(row['Address'], row['Title 1'], row['Meta Description 1']), axis=1)
+        
+        # Define prompts for each page type.
+        prompts = {
+            "Home Page": "Write a compelling meta description for a Home Page with title '{title}' and url '{url}'",
+            "Product Detail Page": "Write a compelling meta description for a Product Detail Page with title '{title}' and url '{url}'",
+            # Add other page types here
+        }
+        
     
     # Generate descriptions for each page type
-    for page_type, prompt_template in prompts.items():
-        df.loc[df['PageType'] == page_type, 'Generated Meta Description'] = df[df['PageType'] == page_type].apply(
-            lambda row: generate_description(prompt_template.format(title=row['Title 1'], url=row['Address'])), 
-            axis=1
-        )
+        for page_type, prompt_template in prompts.items():
+            df.loc[df['PageType'] == page_type, 'Generated Meta Description'] = df[df['PageType'] == page_type].apply(
+                lambda row: generate_description(prompt_template.format(title=row['Title 1'], url=row['Address'])), axis=1)
+        return df
+
+    # Create a file uploader
+    uploaded_file = st.file_uploader("Choose a file", type=['csv', 'xlsx'])
+
+    # If a file is uploaded
+    if uploaded_file is not None:
+        # Load the data
+        if uploaded_file.name.endswith('.csv'):
+            df = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith('.xlsx'):
+            df = pd.read_excel(uploaded_file)
+
+        # Process the data
+        df = process_data(df)
+
+        # Download the processed data
+        csv = df.to_csv(index=False)
+        b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+        href = f'<a href="data:file/csv;base64,{b64}" download="processed_data.csv">Download CSV File</a>'
+        st.markdown(href, unsafe_allow_html=True)
+
+else:
+    # If the password is incorrect, show an error message
+    st.error('The password you entered is incorrect.')
     
-    return df
-
-def download_link(object_to_download, download_filename, download_link_text):
-    # Generate a download link for the processed dataframe
-    if isinstance(objectSure, here is how you can continue the code:
-
-```python
-        if st.button('Download Data'):
-            csv = data.to_csv(index=False)
-            b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-            href = f'<a href="data:file/csv;base64,{b64}" download="processed_data.csv">Download Processed CSV File</a>'
-            st.markdown(href, unsafe_allow_html=True)
- else:
-    # If the password is incorrect, display an error message
-    st.error('The password you entered is incorrect.')   
