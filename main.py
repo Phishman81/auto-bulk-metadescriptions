6894b3ebaee2beb6ac4a2772ad032f77c8d10cbd
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-06-28 09:39:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import openai
 import csv
 import base64
 
-# Load the secrets.toml file
-st.secrets = st.secrets["streamlit"]["secrets"]
-
 # Main page title
 st.title("Auto Metadescriptions Generator")
 
@@ -15,80 +12,75 @@ password = st.secrets["password"]
 password_input = st.text_input("Enter Password", type="password")
 
 if password_input == password:
-
-    # Upload the CSV
-    file = st.file_uploader("Upload a CSV file", type=['csv'])
-
-    # Options for the user
-    option = st.radio("Choose your option:", 
-                      ('Generate Metadescriptions for all URLs', 'Generate Metadescriptions for only SEO Relevant URLs'))
+    # File upload
+    file = st.file_uploader("Upload a CSV File", type=['csv'])
 
     if file is not None:
+        df = pd.read_csv(file)
+
         try:
-            # Load the CSV
-            df = pd.read_csv(file)
-            st.success("File uploaded successfully!")
+            # Check if the CSV file is valid
+            valid_csv = 'url' in df.columns and 'pagetitle' in df.columns and 'metadescription' in df.columns
 
-            # If SEO relevant URLs is selected
-            valid_csv = True
-            if option == 'Generate Metadescriptions for only SEO Relevant URLs':
+            if not valid_csv:
+                st.error("Invalid CSV file. Please ensure it contains the following columns: 'url', 'pagetitle', 'metadescription'.")
+                return
+
+            # Ask if metadescriptions should be generated for all URLs or only SEO relevant ones
+            option = st.selectbox('Do you want to generate metadescriptions for all URLs or only SEO relevant ones?', 
+                                  ('All URLs', 'SEO relevant URLs'))
+
+            if option == 'SEO relevant URLs':
                 if 'Status Code' in df.columns and 'Indexability' in df.columns:
-                    # Filter the dataframe
-                    df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable')]
+                    df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'Indexable')]
                 else:
-                    st.error("The columns 'Status Code' and 'Indexability' are missing. Please select the other option.")
-                    valid_csv = False
+                    proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', 
+                                       ('Yes', 'No'))
+                    if proceed == 'No':
+                        return
 
-            if valid_csv:
-                # Add the pagetype column
-                df['pagetype'] = ''
+            df['pagetype'] = ''
+            df['new metadescription'] = ''
 
-                # Define the page types
-                page_types = ['Home Page', 'Product Detail Page', 'Category Page', 
-                              'About Us Page', 'Contact Us Page', 'Blog Article Page',
-                              'Services Page', 'Landing Page', 'Privacy Policy Page', 
-                              'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
-                              'Portfolio Page', 'Case Study Page', 'Press Release Page', 
-                              'Events Page', 'Resources/Downloads Page', 'Team Members Page',
-                              'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
-                              'Forum/community page', 'News Page']
+            # Use GPT-3 to assign a page type
+            openai.api_key = st.secrets["openai_key"]
 
-                # Use GPT-3 to assign a page type
-                openai.api_key = st.secrets["openai_key"]
+            page_types = ['Home Page', 'Product Detail Page', 'Category Page', 
+                      'About Us Page', 'Contact Us Page', 'Blog Article Page',
+                      'Services Page', 'Landing Page', 'Privacy Policy Page', 
+                      'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
+                      'Portfolio Page', 'Case Study Page', 'Press Release Page', 
+                      'Events Page', 'Resources/Downloads Page', 'Team Members Page',
+                      'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
+                      'Forum/community page', 'News Page']
 
-                st.write("Generating Page types... Please wait.")
-                for i in range(len(df)):
-                    prompt = f"{df.iloc[i, df.columns.get_loc('url')]} {df.iloc[i, df.columns.get_loc('pagetitle')]} {df.iloc[i, df.columns.get_loc('metadescription')]}"
+            for i in range(len(df)):
+                prompt = f"{df.iloc[i, df.columns.get_loc('url')]} {df.iloc[i, df.columns.get_loc('pagetitle')]} {df.iloc[i, df.columns.get_loc('metadescription')}"
 
-                    response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, temperature=0.5, max_tokens=3)
+                response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, temperature=0.5, max_tokens=3)
 
-                    df.iloc[i, df.columns.get_loc('pagetype')] = response.choices[0].text.strip()
-                st.success("Page types generated successfully!")
+                df.iloc[i, df.columns.get_loc('pagetype')] = response.choices[0].text.strip()
 
-                # Add new metadescription column
-                df['new metadescription'] = ''
+            # Add new metadescriptions
+            st.write("Creating new metadescriptions... Please wait.")
+            for i in range(len(df)):
+                prompt = f"{df.iloc[i, df.columns.get_loc('url')]} {df.iloc[i, df.columns.get_loc('pagetitle')]} {df.iloc[i, df.columns.get_loc('metadescription')]}"
 
-                # Use GPT-3 to generate metadescriptions
-                st.write("Generating Metadescriptions... Please wait.")
-                for i in range(len(df)):
-                    prompt = f"{df.iloc[i, df.columns.get_loc('url')]} {df.iloc[i, df.columns.get_loc('pagetitle')]} {df.iloc[i, df.columns.get_loc('metadescription')]}"
+                response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, temperature=0.5, max_tokens=60)
 
-                    response = openai.Completion.create(engine="gpt-3.5-turbo", prompt=prompt, temperature=0.5, max_tokens=60)
+                df.iloc[i, df.columns.get_loc('new metadescription')] = response.choices[0].text.strip()
 
-                    df.iloc[i, df.columns.get_loc('new metadescription')] = response.choices[0].text.strip()
+            st.write("Metadescriptions created.")
 
-                st.success("Metadescriptions generated successfully!")
-
-                # Download the new CSV
-                st.write(df)
-                csv = df.to_csv(index=False)
-                b64 = base64.b64encode(csv.encode()).decode()  
-                href = f'<a href="data:file/csv;base64,{b64}" download="metadescriptions.csv">Download CSV File</a>'
-                st.markdown(href, unsafe_allow_html=True)
+            # Download new CSV
+            csv = df.to_csv(index=False)
+            b64 = base64.b64encode(csv.encode()).decode()  
+            href = f'<a href="data:file/csv;base64,{b64}" download="new_metadescriptions.csv">Download CSV File</a>'
+            st.markdown(href, unsafe_allow_html=True)
 
         except Exception as e:
-            st.error(f"An error occurred: {e}")
+            st.error(f"An error occurred: {str(e)}")
 
 else:
-    st.write("Invalid Password. Please try again.")
-                
+    st.error("Incorrect password. Please try again.")
+            
