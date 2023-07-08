3b42a78af8152a9a1c1f4aea8c0474d41cb7e515
Phishman81 <130157542+Phishman81@users.noreply.github.com>
2023-07-08 02:12:30 +0200

    Update main.py - charts corrected by gpt-4

--- main.py
+++ main.py
@@ -68,130 +68,130 @@ if password_entered and password_input == password:
                     elif option_1 == 'All URLs' and option_2 == 'Only Missing Descriptions':
                         df = df[df['Meta Description 1'].isna() | df['Meta Description 1'] == ""]
 
-                    # Bar plot for URLs with missing meta descriptions
-                    st.subheader("URLs with Missing Meta Descriptions")
-                    missing_desc = df['Meta Description 1'].isna() | df['Meta Description 1'] == ""
-                    fig, ax = plt.subplots()
-                    missing_desc.value_counts().plot(kind='bar', ax=ax)
-                    ax.set_xticklabels(['No Missing Description', 'Missing Description'], rotation=0)
-                    st.pyplot(fig)
-
-                    elif option_1 == 'SEO Relevant URLs' and option_2 == 'All Descriptions':
-                        if 'Status Code' in df.columns and 'Indexability' in df.columns:
-                            df['Indexability'] = df['Indexability'].str.strip().str.lower()
-                            df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable')]
-                            
-                            # Pie chart for SEO relevant vs non-relevant URLs
-                            st.subheader("SEO Relevant vs Non-Relevant URLs")
-                            fig, ax = plt.subplots()
-                            df['Indexability'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax)
-                            st.pyplot(fig)
-                            
-                        else:
-                            proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', ('Yes', 'No'))
-                            if proceed == 'No':
-                                st.warning("Terminating the execution.")
-                                sys.exit()
-
-                    elif option_1 == 'SEO Relevant URLs' and option_2 == 'Only Missing Descriptions':
-                        if 'Status Code' in df.columns and 'Indexability' in df.columns:
-                            df['Indexability'] = df['Indexability'].str.strip().str.lower()
-                            df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable') & (df['Meta Description 1'].isna() | df['Meta Description 1'] == "")]
-                        else:
-                            proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', ('Yes', 'No'))
-                            if proceed == 'No':
-                                st.warning("Terminating the execution.")
-                                sys.exit()
-
-                    if df.empty:
-                        if option_2 == 'Optimize Only Missing Meta Descriptions':
-                            st.warning("There are no missing Meta Descriptions in your selection.")
-                        else:
-                            st.warning("No URLs matching the conditions were found.")
-                        sys.exit()
+                        # Bar plot for URLs with missing meta descriptions
+                        st.subheader("URLs with Missing Meta Descriptions")
+                        missing_desc = df['Meta Description 1'].isna() | df['Meta Description 1'] == ""
+                        fig, ax = plt.subplots()
+                        missing_desc.value_counts().plot(kind='bar', ax=ax)
+                        ax.set_xticklabels(['No Missing Description', 'Missing Description'], rotation=0)
+                        st.pyplot(fig)
 
                 else:
                     st.warning("No URLs with 'text/html' content type found in the CSV.")
                     sys.exit()
 
-                st.write(f"Total URLs to be processed: {len(df)}")
+            elif option_1 == 'SEO Relevant URLs' and option_2 == 'All Descriptions':
+                if 'Status Code' in df.columns and 'Indexability' in df.columns:
+                    df['Indexability'] = df['Indexability'].str.strip().str.lower()
+                    df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable')]
 
-                start_button = st.button(f"Start Processing {len(df)} URLs")
+                    # Pie chart for SEO relevant vs non-relevant URLs
+                    st.subheader("SEO Relevant vs Non-Relevant URLs")
+                    fig, ax = plt.subplots()
+                    df['Indexability'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax)
+                    st.pyplot(fig)
 
-                if start_button:
-                    df['pagetype'] = ''
+                else:
+                    proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', ('Yes', 'No'))
+                    if proceed == 'No':
+                        st.warning("Terminating the execution.")
+                        sys.exit()
 
-                    openai.api_key = st.secrets["openai_key"]
+            elif option_1 == 'SEO Relevant URLs' and option_2 == 'Only Missing Descriptions':
+                if 'Status Code' in df.columns and 'Indexability' in df.columns:
+                    df['Indexability'] = df['Indexability'].str.strip().str.lower()
+                    df = df[(df['Status Code'] == 200) & (df['Indexability'] == 'indexable') & (df['Meta Description 1'].isna() | df['Meta Description 1'] == "")]
+                else:
+                    proceed = st.radio('The CSV does not contain the necessary columns for SEO relevance checking (Status Code and Indexability). Proceed by optimizing all URLs?', ('Yes', 'No'))
+                    if proceed == 'No':
+                        st.warning("Terminating the execution.")
+                        sys.exit()
 
-                    page_types = ['Home Page', 'Product Detail Page', 'Category Page',
-                                'About Us Page', 'Contact Us Page', 'Blog Article Page',
-                                'Services Page', 'Landing Page', 'Privacy Policy Page',
-                                'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
-                                'Portfolio Page', 'Case Study Page', 'Press Release Page',
-                                'Events Page', 'Resources/Downloads Page', 'Team Members Page',
-                                'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
-                                'Forum/community page', 'News Page']
+            if df.empty:
+                if option_2 == 'Optimize Only Missing Meta Descriptions':
+                    st.warning("There are no missing Meta Descriptions in your selection.")
+                else:
+                    st.warning("No URLs matching the conditions were found.")
+                sys.exit()
 
-                    st.write("Defining the pagetype for every URL... Please wait.")
-                    for i in range(len(df)):
-                        address = df.iloc[i]['Address']
-                        title = df.iloc[i]['Title 1']
-                        h1 = df.iloc[i]['H1-1']
-                        meta_description = df.iloc[i]['Meta Description 1']
+            st.write(f"Total URLs to be processed: {len(df)}")
 
-                        prompt = f"You are an SEO expert. From the best of your knowledge, please assign the most suitable pagetype from the following list:\n{page_types}\n\nFor a page with the URL: {address}\nPage a heading: {h1}\n Title: {title}\nMeta Description: {meta_description}\n\nUsing the given information, define the most suitable pagetype from the options provided. Do not write anything else than that."
+            start_button = st.button(f"Start Processing {len(df)} URLs")
 
-                        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=50)
+            if start_button:
+                df['pagetype'] = ''
 
-                        pagetype = response.choices[0].text.strip()
+                openai.api_key = st.secrets["openai_key"]
 
-                        df.iloc[i, df.columns.get_loc('pagetype')] = pagetype
-                        # Bar plot showing the distribution of page types
-                        st.subheader("Distribution of Page Types")
-                        fig, ax = plt.subplots()
-                        sns.countplot(data=df, x='pagetype', ax=ax)
-                        plt.xticks(rotation=90)
-                        st.pyplot(fig)
+                page_types = ['Home Page', 'Product Detail Page', 'Category Page',
+                            'About Us Page', 'Contact Us Page', 'Blog Article Page',
+                            'Services Page', 'Landing Page', 'Privacy Policy Page',
+                            'Terms and Conditions Page', 'FAQ Page', 'Testimonials Page',
+                            'Portfolio Page', 'Case Study Page', 'Press Release Page',
+                            'Events Page', 'Resources/Downloads Page', 'Team Members Page',
+                            'Careers/Jobs Page', 'Login/Register Page', 'E-commerce shopping cart page',
+                            'Forum/community page', 'News Page']
 
-                    st.write("Intermediate Result - Processed URLs with their Pagetypes:")
-                    processed_urls_df = df[['Content Type', 'Address', 'Title 1', 'Meta Description 1', 'H1-1', 'pagetype','Status Code', 'Indexability']]
-                    st.dataframe(processed_urls_df)
+                st.write("Defining the pagetype for every URL... Please wait.")
+                for i in range(len(df)):
+                    address = df.iloc[i]['Address']
+                    title = df.iloc[i]['Title 1']
+                    h1 = df.iloc[i]['H1-1']
+                    meta_description = df.iloc[i]['Meta Description 1']
 
-                    st.write("Generating new metadescriptions for every URL... Please wait.")
-                    df['new_metadescription'] = ''
+                    prompt = f"You are an SEO expert. From the best of your knowledge, please assign the most suitable pagetype from the following list:\n{page_types}\n\nFor a page with the URL: {address}\nPage a heading: {h1}\n Title: {title}\nMeta Description: {meta_description}\n\nUsing the given information, define the most suitable pagetype from the options provided. Do not write anything else than that."
 
-                    for i in range(len(df)):
-                        address = df.iloc[i]['Address']
-                        title = df.iloc[i]['Title 1']
-                        h1 = df.iloc[i]['H1-1']
-                        meta_description = df.iloc[i]['Meta Description 1']
-                        pagetype = df.iloc[i]['pagetype']
+                    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=50)
 
-                        prompt = f""" You are an SEO Expert who crafts excellent meta descriptions. Generate a concise and engaging meta description of maximum 150 characters for a webpage of type '{pagetype}', with the URL '{address}', page title '{title}', the main heading '{h1}' and a current meta description that looks like the following but needs improvement '{meta_description}'. """
+                    pagetype = response.choices[0].text.strip()
 
-                        response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=80)
+                    df.iloc[i, df.columns.get_loc('pagetype')] = pagetype
+                    # Bar plot showing the distribution of page types
+                    st.subheader("Distribution of Page Types")
+                    fig, ax = plt.subplots()
+                    sns.countplot(data=df, x='pagetype', ax=ax)
+                    plt.xticks(rotation=90)
+                    st.pyplot(fig)
 
-                        new_metadescription = response.choices[0].text.strip()
+                st.write("Intermediate Result - Processed URLs with their Pagetypes:")
+                processed_urls_df = df[['Content Type', 'Address', 'Title 1', 'Meta Description 1', 'H1-1', 'pagetype','Status Code', 'Indexability']]
+                st.dataframe(processed_urls_df)
 
-                        df.iloc[i, df.columns.get_loc('new_metadescription')] = new_metadescription
+                st.write("Generating new metadescriptions for every URL... Please wait.")
+                df['new_metadescription'] = ''
 
-                        # Histogram for meta description lengths
-                        st.subheader("Meta Description Lengths")
-                        fig, ax = plt.subplots()
-                        df['new_metadescription'].str.len().plot(kind='hist', bins=30, ax=ax)
-                        ax.set_xlabel("Meta Description Length")
-                        st.pyplot(fig)
+                for i in range(len(df)):
+                    address = df.iloc[i]['Address']
+                    title = df.iloc[i]['Title 1']
+                    h1 = df.iloc[i]['H1-1']
+                    meta_description = df.iloc[i]['Meta Description 1']
+                    pagetype = df.iloc[i]['pagetype']
+
+                    prompt = f""" You are an SEO Expert who crafts excellent meta descriptions. Generate a concise and engaging meta description of maximum 150 characters for a webpage of type '{pagetype}', with the URL '{address}', page title '{title}', the main heading '{h1}' and a current meta description that looks like the following but needs improvement '{meta_description}'. """
+
+                    response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, temperature=0.5, max_tokens=80)
+
+                    new_metadescription = response.choices[0].text.strip()
+
+                    df.iloc[i, df.columns.get_loc('new_metadescription')] = new_metadescription
+
+                    # Histogram for meta description lengths
+                    st.subheader("Meta Description Lengths")
+                    fig, ax = plt.subplots()
+                    df['new_metadescription'].str.len().plot(kind='hist', bins=30, ax=ax)
+                    ax.set_xlabel("Meta Description Length")
+                    st.pyplot(fig)
 
 
-                    st.write("Result - Processed URLs with their Pagetypes and New Metadescriptions:")
-                    processed_urls_df = df[['Content Type', 'Address', 'Title 1', 'Meta Description 1', 'pagetype', 'new_metadescription']]
-                    st.dataframe(processed_urls_df)
+                st.write("Result - Processed URLs with their Pagetypes and New Metadescriptions:")
+                processed_urls_df = df[['Content Type', 'Address', 'Title 1', 'Meta Description 1', 'pagetype', 'new_metadescription']]
+                st.dataframe(processed_urls_df)
 
-                    st.success("Metadescriptions have been created successfully! You can download the updated CSV below.")
-                    csv = df.to_csv(index=False)
-                    b64 = base64.b64encode(csv.encode()).decode()
-                    href = f'<a href="data:file/csv;base64,{b64}" download="new_metadescriptions.csv">Download CSV File</a>'
-                    st.markdown(href, unsafe_allow_html=True)
+                st.success("Metadescriptions have been created successfully! You can download the updated CSV below.")
+                csv = df.to_csv(index=False)
+                b64 = base64.b64encode(csv.encode()).decode()
+                href = f'<a href="data:file/csv;base64,{b64}" download="new_metadescriptions.csv">Download CSV File</a>'
+                st.markdown(href, unsafe_allow_html=True)
 
         except Exception as e:
             st.error(f"An error occurred: {str(e)}")
